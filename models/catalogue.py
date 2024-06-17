14ac699b2ad85305a6e5749063dbb98c14850580
Denis <mugambimuthuri0@gmail.com>
2024-06-17 18:26:34 +0300

    setup models

--- models/catalogue.py
+++ models/catalogue.py
@@ -1,22 +1,21 @@
-from db import cursor, conn
+# models/catalogue.py
+
+from db import cursor
 
 class Model:
     TABLE_NAME = 'models'
-    def __init__(self, name) -> None:
+
+    def __init__(self, name):
         self.id = None
         self.name = name
 
     def save(self):
-        # Check if the name already exists
         cursor.execute(f"SELECT id FROM {self.TABLE_NAME} WHERE name = ?", (self.name,))
         result = cursor.fetchone()
         if result:
             print(f"{self.name} already exists with id {result[0]}")
         else:
-            sql = f"""
-                INSERT INTO {self.TABLE_NAME} (name)
-                VALUES (?)
-            """
+            sql = f"INSERT INTO {self.TABLE_NAME} (name) VALUES (?)"
             cursor.execute(sql, (self.name,))
             conn.commit()
             self.id = cursor.lastrowid
@@ -30,41 +29,35 @@ class Model:
 
     @classmethod
     def find_all(cls):
-        sql = f"""
-        SELECT * FROM {cls.TABLE_NAME}
-        """
-
-        rows = cursor.execute(sql).fetchall()
-
-        return [
-            [cls.row_to_instance(row).to_dict() for row in rows]
-        ]
+        try:
+            sql = f"SELECT * FROM {cls.TABLE_NAME}"
+            cursor.execute(sql)
+            rows = cursor.fetchall()
+            models = [cls.row_to_instance(row).to_dict() for row in rows]
+            print(f"Fetched {len(models)} models from database")
+            return models
+        except Exception as e:
+            print(f"Error fetching models: {e}")
+            return []
 
     @classmethod
     def row_to_instance(cls, row):
-        if row == None:
+        if row is None:
             return None
 
         model = cls(row[1])
         model.id = row[0]
 
         return model
+
     @classmethod
     def create_table(cls):
         sql = f"""
             CREATE TABLE IF NOT EXISTS {cls.TABLE_NAME} (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 name TEXT NOT NULL UNIQUE
-                )
-            """
+            )
+        """
         cursor.execute(sql)
         conn.commit()
         print(f"Models table created")
-
-Model.create_table()
-
-# models = ["Kawasaki","Yamaha","BMW","Jincheng","Taro"]
-
-# for model_name in models:
-#     model = Model(model_name)
-#     model.save()
